adabbcfc4992f9d96881abbe8db88d49d88f2a27
Nanaya Miki <nanayamiki@gmail.com>
2023-04-08 16:47:01 +0000

    completed the challenge. had help from a mentor Matthew>

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+	// Sum all the numbers in the arra
+	let result = 0;
+	for (item of array) {
+		if (Array.isArray(item)) {
+			result += sumItems(item);
+		} else {
+			result += item;
+		}
+	}
+	console.log(result);
+	return result;
 }
+sumItems([[1, 2, [[3], 4]], 5, []]);
 
 module.exports = sumItems;
